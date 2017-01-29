17d7073b2df4d1d166495fffc09317d070916c3e
Karsten Schmidt <k@postspectacular.com>
2017-01-29 12:18:33 +0000

    initial import

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject thi.ng/punchcard "0.1.0-SNAPSHOT"
+  :description  "Git repo commit visualization"
+  :url          "http://thi.ng/punchcard"
+  :license      {:name "Apache Software License 2.0"
+                 :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [thi.ng/geom "0.0.1178-SNAPSHOT"]]
+  :main         thi.ng.punchcard.core)
