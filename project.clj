a3c465fc6fb97fd80435f5fa36cc3cc31c555be8
Karsten Schmidt <k@postspectacular.com>
2017-01-29 13:11:04 +0000

    update readme & assets

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject thi.ng/punchcard "0.1.0-SNAPSHOT"
-  :description  "Git repo commit visualization"
+  :description  "Customizable Git commit punchcard visualizations"
   :url          "http://thi.ng/punchcard"
   :license      {:name "Apache Software License 2.0"
                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
