612f16cc918749a9364c055eec7f9ac004e963f8
Karsten Schmidt <k@postspectacular.com>
2017-11-09 11:55:17 +0000

    feat: add ramp, bg,fg & reverse opts, update thi.ng/color dep

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license      {:name "Apache Software License 2.0"
                  :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [thi.ng/geom "0.0.1178-SNAPSHOT"]]
+                 [thi.ng/geom "0.0.1178-SNAPSHOT"]
+                 [thi.ng/color "1.2.1-SNAPSHOT"]]
   :main         thi.ng.punchcard.core)
